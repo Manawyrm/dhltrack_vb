6499b062ca5ec9225f2874148d3bbe8d4f51d3ac
Tobias Mädel <t.maedel@alfeld.de>
2013-01-15 10:12:54 +0100

    first commit. should (xD) work.

---
+++ dhltrack_vb/Main.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Collections.Generic
+Imports System.Text
+
+Public Class Main
+    Public Shared Sub Main()
+        'Inkorrekte Argumente
+        If My.Application.CommandLineArgs.Count <> 1 Then
+            'Falsche Anzahl der Argumente
+            Console.WriteLine("Benutzung: dhltrack [Delivery-ID]")
+        Else
+            'The joy of working with modern languages...
+            Dim id As String = My.Application.CommandLineArgs(0)
+            'Building up the URL...
+            Dim url As String = "http://nolp.dhl.de/nextt-online-public/set_identcodes.do?lang=de&idc=" & id
+
+            Dim wc As New System.Net.WebClient()
+            wc.Encoding = UTF8Encoding.UTF8
+            Dim htmldata As String = wc.DownloadString(url)
+
+            If htmldata.Contains("<div class=""error"">") Then
+                'DHL gibt bei nicht vorhandener ID den Error in dieser CSS-Klasse heraus.
+                'Leider nicht vorhanden.
+                Console.WriteLine("Es ist keine Sendung mit der ID " & id & " bekannt!")
+            Else
+                'Status der Sendung extrahieren -- evtl. wäre hier ein RegExp besser... 
+                Dim status As String = Split(Split(htmldata, "<td class=""status"">")(1), "</td>")(0).Replace("<div class=""statusZugestellt"">", "").Replace("</div>", "").Trim()
+                Console.WriteLine("Status der Sendung mit ID: " & id)
+                Console.WriteLine(status)
+            End If
+        End If
+
+    End Sub
+End Class
---
+++ dhltrack_vb/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("dhltrack_vb")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("tbspace.de")> 
+<Assembly: AssemblyProduct("dhltrack_vb")> 
+<Assembly: AssemblyCopyright("Copyright © tbspace.de 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("74e27f15-be7c-46c2-a9e4-e78c803056a3")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
